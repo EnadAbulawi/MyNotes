d987f9ac0e223c8b9339a66e5600df28261ba5d5
EnadAbulawi <enadabulawi7@gmail.com>
2023-07-21 01:21:15 +0300

    build app ui

---
+++ lib/Views/notes_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../Widgets/notes_view_body.dart';
+
+class NotewsView extends StatelessWidget {
+  const NotewsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const NotesViewBody();
+  }
+}
---
+++ lib/Widgets/custom_appbar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        CustomSearchIcon(),
+        Spacer(),
+        Text(
+          'مذكراتي',
+          style: TextStyle(fontSize: 18),
+        ),
+      ],
+    );
+  }
+}
