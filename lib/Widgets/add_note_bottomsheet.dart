c1a6b726489bc6859eabbac96e47c36fa97292f7
EnadAbulawi <enadabulawi7@gmail.com>
2023-07-21 20:12:55 +0300

    edit in bottomsheet

--- lib/Widgets/add_note_bottomsheet.dart
+++ lib/Widgets/add_note_bottomsheet.dart
@@ -1,11 +1,31 @@
-
 import 'package:flutter/material.dart';
+
+import 'customtextfiled.dart';
 
 class AddNoteBootomSheet extends StatelessWidget {
   const AddNoteBootomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextFiled(
+            hint: "عنوان المذكرة",
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextFiled(
+            hint: "محتوى المذكرة",
+            maxLines: 7,
+          )
+        ],
+      ),
+    );
   }
 }
